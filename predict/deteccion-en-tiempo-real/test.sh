ee2e194320eefd8e5c01829f5a613ba0d78d6c2e
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2025-11-15 23:27:04 +0000

    test sh working full fps now test with our model

---
+++ predict/deteccion-en-tiempo-real/test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+t=ultralytics/ultralytics:latest-jetson-jetpack6
+sudo docker pull $t
+
+xhost +local:docker
+
+sudo docker run -it --ipc=host \
+  --runtime=nvidia \
+  --gpus all \
+  -v /home/$USER:/home/$USER \
+  --device=/dev/video0:/dev/video0 \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  $t bash -c "\
+    cd /home/$USER/projects/universidad/bfmc/Robot-Autonomo-de-Laboratorio-BFMC/vision-artificial/predict/deteccion-en-tiempo-real && \
+    apt-get update && apt-get install -y \
+      libqt5x11extras5 \
+      libxcb-icccm4 \
+      libxcb-image0 \
+      libxcb-keysyms1 \
+      libxcb-randr0 \
+      libxcb-render-util0 \
+      libxcb-xinerama0 \
+      libxkbcommon-x11-0 \
+      x11-xserver-utils \
+      python3-opencv && \
+    yolo export model=weights/merged/best.pt format=engine && \
+    yolo predict model=weights/merged/best.engine source=0 show=True"
