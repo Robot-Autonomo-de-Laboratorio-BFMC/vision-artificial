6b3fad43980a6334a3f311978658735b76415c90
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2025-11-18 18:25:52 +0000

    add metadata for yolo

--- predict/deteccion-en-tiempo-real/docker/predict_without_opencv_windows.sh
+++ predict/deteccion-en-tiempo-real/docker/predict_without_opencv_windows.sh
@@ -30,4 +30,4 @@ sudo docker run -it --ipc=host \
       x11-xserver-utils \
       python3-opencv && \
     ([ -f weights/merged/best.engine ] && echo 'Engine file already exists, skipping export' || yolo export model=weights/merged/best.pt format=engine) && \
-    yolo predict model=weights/merged/best.engine source=0 show=True"
+    yolo predict model=weights/merged/best.engine source=0 show=True verbose=True"
